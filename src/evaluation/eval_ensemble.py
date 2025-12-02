b88a160d3e48f89f02095c3214d5f7e4f20e7b45
manuelmmc <101414339+manuelmmc@users.noreply.github.com>
2025-12-02 20:12:57 +0100

    Rename eval_ensemble_ctrl_plus_cls_2.py to eval_ensemble.py

--- src/evaluation/eval_ensemble.py
+++ src/evaluation/eval_ensemble.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# scripts_nuevos/eval_ensemble_ctrl_plus_cls_2.py
+# src/evaluation/eval_ensemble.py
 """
 Evalúa un ensemble: acciones = clip( a_controlador + a_clasificador , -1, 1 )
 
