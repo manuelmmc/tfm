9e732f3ba4af8b8a519bee8760f93f38071f4f38
manuelmmc <101414339+manuelmmc@users.noreply.github.com>
2025-12-02 20:18:13 +0100

    Add train_rssm_v2.py to joint models

--- src/models/joint/train_rssm_v2.py
+++ src/models/joint/train_rssm_v2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# scripts_nuevos/train_joint_worldmodel_controller_completo_2.py
+# src/models/joint/train_rssm_v2.py
 """
 Entrenamiento conjunto Controller + World Model + (opcional) Clasificador para series sintéticas 24/7.
 
